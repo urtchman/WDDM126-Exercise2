a8ee4a7b7ff1f7e1a26ece4946c4a731cd0aed22
urtchman <urtchman04@yahoo.co.uk>
2024-09-22 22:36:44 -0400

    Sixth commit - added showDateTime() to main.js and created date-time div in the index.html page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function addTrip(phone){
   console.log("Add trip using the number "+phone);
 }
+
+function showDateTime() {
+  var now = new Date();
+  var dateTimeString = now.toLocaleString(); // Format the date and time
+  document.getElementById("thisdate").innerHTML = dateTimeString;
+}
